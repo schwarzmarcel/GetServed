db5c59bcc45e52d63531f78b6f38f9df4e4cf6c0
Marcel <Marcel@10.253.236.168>
2019-09-19 16:38:12 +0200

    fullscreen

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -9,8 +9,8 @@ import com.mygdx.game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 800; 
-		config.height = 450;
+		config.width = LwjglApplicationConfiguration.getDesktopDisplayMode().width; 
+		config.height = LwjglApplicationConfiguration.getDesktopDisplayMode().height;
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
