8a43da42611bf01f477af1179cf08a2419e1c3d0
Geddi <e1626508@student.tuwien.ac.at>
2019-09-19 16:20:36 +0200

    implemented the box class

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -4,24 +4,38 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import entities.Box;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
+	private Texture img;
+	private World world;
+	private Sprite sprite;
+	private Box box;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		sprite = new Sprite(img);
+		sprite.setPosition(Gdx.graphics.getWidth() / 2 - sprite.getWidth() / 2,
+				Gdx.graphics.getHeight() / 2);
+		world = new World(new Vector2(0, 0), true);
+		box = new Box(true,sprite.getX(), sprite.getY(), sprite.getWidth()/2,sprite.getHeight()/2,world);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		world.step(Gdx.graphics.getDeltaTime(), 6, 2);
+		sprite.setPosition(box.getBody().getPosition().x,box.getBody().getPosition().y);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(sprite, sprite.getX(), sprite.getY());
 		batch.end();
 	}
 	
--- core/src/entities/Box.java
+++ core/src/entities/Box.java
@@ -1,4 +1,74 @@
 package entities;
 
+import com.badlogic.gdx.physics.box2d.*;
+
 public class Box {
+    private BodyDef bodyDef;
+    private float positionX;
+    private float positionY;
+    private float boxWidth;
+    private float boxHeight;
+    private Body body;
+
+    public Box(boolean isDynamic, float positionX, float positionY, float boxWidth, float boxHeight, World world) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.boxHeight = boxHeight;
+        this.boxWidth = boxWidth;
+        BodyDef bodyDef = new BodyDef();
+        if(isDynamic){
+            bodyDef.type = BodyDef.BodyType.DynamicBody;
+        }else {
+            bodyDef.type = BodyDef.BodyType.StaticBody;
+        }
+        bodyDef.position.set(positionX, positionY);
+        body = world.createBody(bodyDef);
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(boxWidth, boxHeight);
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.density = 1f;
+        body.createFixture(fixtureDef);
+        shape.dispose();
+    }
+
+    public double getPositionX() {
+        return positionX;
+    }
+
+    public void setPositionX(float positionX) {
+        this.positionX = positionX;
+    }
+
+    public double getPositionY() {
+        return positionY;
+    }
+
+    public void setPositionY(float positionY) {
+        this.positionY = positionY;
+    }
+
+    public BodyDef getBodyDef() {
+        return bodyDef;
+    }
+
+    public Body getBody() {
+        return body;
+    }
+
+    public float getBoxWidth() {
+        return boxWidth;
+    }
+
+    public void setBoxWidth(float boxWidth) {
+        this.boxWidth = boxWidth;
+    }
+
+    public float getBoxHeight() {
+        return boxHeight;
+    }
+
+    public void setBoxHeight(float boxHeight) {
+        this.boxHeight = boxHeight;
+    }
 }
