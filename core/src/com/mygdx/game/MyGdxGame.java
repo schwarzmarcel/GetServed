bf3fe13ff02c7da2956c6ed769d562715b3f300e
Geddi <e1626508@student.tuwien.ac.at>
2019-09-19 20:43:26 +0200

    fixed shitty movement

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -3,61 +3,75 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.math.Matrix4;
 import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.*;
 import entities.Box;
 import entities.Waiter;
 
 public class MyGdxGame extends ApplicationAdapter {
-	private SpriteBatch batch;
-	private World world;
-	private Waiter waiter;
-	
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		world = new World(new Vector2(0, 0), true);
-		waiter = new Waiter(world);
-	
-		
-	}
 
-	@Override
-	public void render () {
-		world.step(Gdx.graphics.getDeltaTime(), 6, 2);
-		waiter.getSprite().setPosition(waiter.getBox().getBody().getPosition().x,waiter.getBox().getBody().getPosition().y);
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		move();
-		batch.begin();
-		batch.draw(waiter.getSprite(), waiter.getSprite().getX(), waiter.getSprite().getY(),Gdx.graphics.getWidth() / 6, Gdx.graphics.getHeight() / 5);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		
-		
-	}
-	
-	public void move() {
-		int horizontalForce = 0;
-		
-		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-			horizontalForce += 1;
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-			horizontalForce -= 1;
-		}
-		
-		waiter.getBox().getBody().setLinearVelocity(horizontalForce * 5000, waiter.getBox().getBody().getLinearVelocity().y);
-	}
+    private SpriteBatch batch;
+    private World world;
+    private Box2DDebugRenderer debugRenderer;
+    private OrthographicCamera camera;
+    private Waiter waiter;
+    private Waiter waiter2;
+    public static final float PIXELS_TO_METERS = 100f;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        world = new World(new Vector2(0, 0), true);
+        waiter = new Waiter(world, -700,-100);
+        waiter2 = new Waiter(world,200,-300);
+
+        debugRenderer = new Box2DDebugRenderer();
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.
+                getHeight());
+    }
+    @Override
+    public void render() {
+        camera.update();
+        world.step(1f / 60f, 6, 2);
+
+        ((Sprite)waiter.getBody().getUserData()).setPosition(
+                (waiter.getBody().getPosition().x * PIXELS_TO_METERS) - waiter.getSprite().getWidth() / 2,
+                (waiter.getBody().getPosition().y * PIXELS_TO_METERS) - waiter.getSprite().getHeight() / 2
+        );
+
+        ((Sprite)waiter2.getBody().getUserData()).setPosition(
+                (waiter2.getBody().getPosition().x * PIXELS_TO_METERS) - waiter2.getSprite().getWidth() / 2,
+                (waiter2.getBody().getPosition().y * PIXELS_TO_METERS) - waiter2.getSprite().getHeight() / 2
+        );
+
+        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.setProjectionMatrix(camera.combined);
+        Matrix4 debugMatrix = batch.getProjectionMatrix().cpy().scale(PIXELS_TO_METERS,
+                PIXELS_TO_METERS, 0);
+        waiter.move();
+        batch.begin();
+        batch.draw(waiter.getSprite(),
+                waiter.getSprite().getX(),
+                waiter.getSprite().getY()
+        );
+        batch.draw(waiter2.getSprite(),
+                waiter2.getSprite().getX(),
+                waiter2.getSprite().getY()
+        );
+        batch.end();
+        if(true) debugRenderer.render(world, debugMatrix);
+    }
+
+    @Override
+    public void dispose() {
+        world.dispose();
+    }
 }
--- core/src/entities/Waiter.java
+++ core/src/entities/Waiter.java
@@ -1,55 +1,60 @@
 package entities;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.*;
+
+import static com.mygdx.game.MyGdxGame.PIXELS_TO_METERS;
+
 
 public class Waiter {
-    private Sprite sprite;
     private TextureAtlas textureAtlas;
     private TextureRegion textureRegion;
-    private Box box;
-
-    public Waiter(World world) {
+    private Sprite sprite;
+    private Body body;
+    public Waiter(World world, float positionX, float positionY) {
         textureAtlas = new TextureAtlas(Gdx.files.internal("spritesheets/avatarsprites.atlas"));
         textureRegion = textureAtlas.findRegion("6_ATTACK");
         sprite = new Sprite(textureRegion);
-        sprite.setPosition(Gdx.graphics.getWidth() / 2 - sprite.getWidth() / 2,
-                Gdx.graphics.getHeight() / 2);
-        box = new Box(true,sprite.getX(), sprite.getY(), sprite.getWidth()/2,sprite.getHeight()/2,world);
+        sprite.setPosition(positionX,positionY);
+
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+
+        bodyDef.position.set((sprite.getX() + sprite.getWidth() / 2) / PIXELS_TO_METERS, (sprite.getY() + sprite.getHeight() / 2) / PIXELS_TO_METERS);
+        body = world.createBody(bodyDef);
+        body.setUserData(sprite);
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox((sprite.getWidth()/2) / PIXELS_TO_METERS,(sprite.getHeight()/2) / PIXELS_TO_METERS);
+        System.out.println(sprite.getWidth());
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        body.createFixture(fixtureDef);
+        shape.dispose();
+    }
+
+    public void move() {
+        float velX = 0, velY = 0;
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            velY = 2.0f;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            velX = 2.0f;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            velY = -2.0f;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            velX = -2.0f;
+        }
+        body.setLinearVelocity(velX, velY);
     }
 
     public Sprite getSprite() {
         return sprite;
     }
 
-    public void setSprite(Sprite sprite) {
-        this.sprite = sprite;
-    }
-
-    public TextureAtlas getTextureAtlas() {
-        return textureAtlas;
-    }
-
-    public void setTextureAtlas(TextureAtlas textureAtlas) {
-        this.textureAtlas = textureAtlas;
-    }
-
-    public TextureRegion getTextureRegion() {
-        return textureRegion;
-    }
-
-    public void setTextureRegion(TextureRegion textureRegion) {
-        this.textureRegion = textureRegion;
-    }
-
-    public Box getBox() {
-        return box;
-    }
-
-    public void setBox(Box box) {
-        this.box = box;
+    public Body getBody() {
+        return body;
     }
 }
