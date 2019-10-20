cc94f8fea63d46d8b0cde63cde2021acf910416b
Marcel <Marcel@192.168.1.66>
2019-10-20 18:02:56 +0200

    level 3

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -34,7 +34,7 @@ public class MyGdxGame extends Game {
         camera.update();
         batch.setProjectionMatrix(camera.combined);
         shapeRenderer.setProjectionMatrix(camera.combined);
-        levelCount = 2;
+        levelCount = 3;
         Assets.loadingScreen();
         Assets.manager.finishLoading();
         loadingScreen = new LoadingScreen(this, batch);
